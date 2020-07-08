dab7545928a0d60a5de344210a697a6709bbfb0e
bhavikaarora <50354230+bhavikaarora@users.noreply.github.com>
2020-07-08 22:28:33 +0530

    Create reverse-LL.java

---
+++ Linked List/reverse-LL.java
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+class ReverseLL
+{
+    // This function should reverse linked list and return
+    // head of the modified linked list.
+    Node reverseList(Node head)
+    {
+        // code here
+        Node p = head;
+        Node q = null;
+        Node r = null;
+        while(p!=null){
+            r = q;
+            q = p;
+            p = p.next;
+            q.next = r;
+        }
+        head = q;
+        return q;
+        
+    }
+}
